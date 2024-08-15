db15906f1ddc0a09199c5bf6d9c9965013495cbb
Beyond DAO <14137098+RetroSoftLabs@users.noreply.github.com>
2024-08-15 14:05:58 +0300

    Initial commit

---
+++ src/consts/app.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = 'Mercury';
+export const APP_DESCRIPTION = 'Security and Interoperability';
+export const APP_URL = 'hyperlane-warp-template.vercel.app';
+export const APP_BRAND_COLOR = '#025aa1';
